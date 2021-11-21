b4fc98a5787dcb0cbf60292ace4c080cd5a5c5a9
Mail I <maili@merunoMacBook-Air.local>
2021-11-21 15:05:36 +0900

    test

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ let myChart = document.getElementById("myChart").getContext('2d');
 let hoge = new Chart(myChart,{
   type:'bar',
   data:{
-    labels:["naniwa","aho","sasa"],
+    labels:["naniwa","aho","sasa","sasaa"],
     datasets:[{
       label:'Name',
       data:[
         22,
         21,
-        31
+        31,
+        21
       ],
       backgroundColor:'green'
     }]
